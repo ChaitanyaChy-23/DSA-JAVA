8eaf7d9632f239132369ba2dcc028b830d649701
Chaitanya <pchaitanyababu@gmail.com>
2026-05-21 10:09:53 +0530

    Two sum

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,3 +1,23 @@
 // LeetCode 1 - Two Sum
-// Topic: Arrays + HashMap
+// Topic: Arrays 
 // Difficulty: Easy
+import java.util.*;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int sum = 0;
+        
+        for(int i = 0;i<nums.length;i++){
+            for(int j = i+1;j<nums.length;j++){
+                sum = nums[i]+nums[j];
+                if(sum == target){
+                    return new int[]{i,j};
+                    
+                }
+            }
+        }
+
+    return new int[]{};
+    }
+    
+}
